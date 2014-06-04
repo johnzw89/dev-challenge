73e188737da3a22f11d5a3ab6030d75bbdc2d0c5
john wei <johnzw89@gmail.com>
2014-06-03 23:31:41 -0400

    2 and 3

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,54 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name: John Wei
+
+# 2.1
+# zipcode = 02492
+
+# >>> 01
+# 1
+# >>> 010
+# 8
+
+# >>> 0100
+# 64
+# >>> 01000
+# 512
+# >>> 010000
+# 4096
+
+# http://stackoverflow.com/questions/11620151/what-do-numbers-starting-with-0-mean-in-python
+
+
+# 2.2
+# 5
+# print 5
+# x = 5
+# print x
+# x + 1
+# # if you want to x to be 6, x = x + 1
+# print x
+# x = x + 1
+# print x
+
+# 2.3
+# Assume that we execute the following assignment statements:
+# width = 17
+# height = 12.0
+# delimiter = '.'
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+# width/2
+8 # because 12 is not a float, so answer is integer
+# width/2.0
+7.5 #float
+# height/3
+36.0
+# 1 + 2 * 5
+11 #integer
+# delimiter * 5
+'.....' #string
+
+
+# 2.4
+# user floats
+# answer = (4.0/3.0)*3.14*5.0**3.0 = 523.3334
--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,81 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name: John Wei
+
+# 1
+
+# repeat_lyrics()
+# def print_lyrics():
+#     print "I'm a lumberjack, and I'm okay."
+#     print "I sleep all night and I work all day."
+
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+
+# it doesnt work because you are calling the function before its defined
+
+# 2
+
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+
+# def print_lyrics():
+#     print "I'm a lumberjack, and I'm okay."
+#     print "I sleep all night and I work all day."
+
+# repeat_lyrics()
+# no change
+
+# 3
+
+# def right_justify(s):
+#   columns =  70
+#   word_length = len(s)
+#   justified_word = (columns - word_length)* ' ' + s
+#   print justified_word
+
+# right_justify('blllgggaahh')
+# remember it is the last letter that needs to be at col 70
+
+#4
+
+# def do_twice(f,value):
+#     f(value)
+#     f(value)
+
+# def print_spam(value):
+#     print value
+
+# do_twice(print_spam, 'spam!')
+
+# def print_twice(a):
+#   do_twice(print_spam,a)
+
+# print_twice('hotdogs!')
+
+#5
+
+def draw_h():
+  line = '+ - - - - + - - - - +'
+  print line
+
+def draw_v():
+  col = '|         |         |'
+  print col
+
+def draw_square():
+  draw_h()
+  draw_v()
+  draw_v()
+  draw_v()
+  draw_v()
+  draw_h()
+  draw_v()
+  draw_v()
+  draw_v()
+  draw_v()
+  draw_h()
+
+draw_square()
